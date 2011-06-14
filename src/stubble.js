b39a7c8b254725cdcb17bdbb1bbb665d135f83ee
Joshua Tacoma <joshua@yellowseed.org>
2011-06-13 21:53:01 -0400

    more robust culture mapping.

--- src/stubble.js
+++ src/stubble.js
@@ -12,14 +12,17 @@ function stubble(arg) {
   arg = arg || {};
   if (typeof this !== 'undefined' && this !== null && !arg.target)
     arg.target = this;
+  if (arg.culture)
+    arg.culture = arg.culture.replace('-', '_');
   for (var sourceName in arg.source)
   {
-    var parts = sourceName.split('_');
+    var parts = sourceName.split('_', 3);
     var indexName = parts[0];
     var targetName = sourceName;
     if (arg.culture && parts.length > 2)
     {
-      if (parts[2] != arg.culture) continue;
+      if (parts[2] != arg.culture.substring(0, parts[2].length))
+          continue;
       targetName = parts[0] + '_' + parts[1];
     }
     var index = arg.target[indexName];
